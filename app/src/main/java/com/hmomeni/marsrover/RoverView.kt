0da769eb9c2331c89752a0fa13eb44f013f4543c
Hamed Momeni <hamed.ma7@gmail.com>
2018-12-09 10:35:00 +0330

    adding comments and reordeing the RoverView

--- app/src/main/java/com/hmomeni/marsrover/RoverView.kt
+++ app/src/main/java/com/hmomeni/marsrover/RoverView.kt
@@ -18,6 +18,14 @@ class RoverView : View {
     constructor(context: Context) : super(context)
     constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet)
 
+
+    var roverPosition = Point(0, 0)
+    var blockedCells = Array(20) {
+        return@Array Array(10) {
+            false
+        }
+    }
+
     private var roverUp: Bitmap = BitmapFactory.decodeResource(resources, R.drawable.rover_up)
     private var roverRight: Bitmap = BitmapFactory.decodeResource(resources, R.drawable.rover_right)
     private var roverLeft: Bitmap = BitmapFactory.decodeResource(resources, R.drawable.rover_left)
@@ -39,15 +47,17 @@ class RoverView : View {
         textSize = dpToPx(14).toFloat()
     }
 
-    var blockedCells = Array(20) {
-        return@Array Array(10) {
-            false
-        }
-    }
 
+    private val viewRect: RectF = RectF()
 
-    var roverPosition = Point(0, 0)
     private lateinit var roverRect: RectF
+
+    private var direction = UP
+    private var textBubbleRect: RectF? = null
+    private var textPos: Pair<Float, Float>? = null
+    private var message: String = ""
+    private var cancelMovement = false
+
 
     fun reset() {
         textBubbleRect = null
@@ -69,68 +79,6 @@ class RoverView : View {
         invalidate()
     }
 
-    private fun calculateRoverRect() {
-        roverRect = RectF(
-            roverPosition.x * cellWidth + cellPadding,
-            (19 - roverPosition.y) * cellWidth + cellPadding,
-            roverPosition.x * cellWidth + cellWidth,
-            (19 - roverPosition.y) * cellWidth + cellWidth
-        )
-    }
-
-    private val viewRect: RectF = RectF()
-
-    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-        if (measuredWidth > 0 && measuredHeight > 0) {
-            cellWidth = min((measuredWidth - 11 * cellPadding) / 9, (measuredHeight - 11 * cellPadding) / 19)
-            setMeasuredDimension(measuredWidth, measuredHeight)
-            viewRect.set(0f, 0f, measuredWidth.toFloat(), measuredHeight.toFloat())
-            calculateRoverRect()
-        }
-    }
-
-    override fun onDraw(canvas: Canvas) {
-        for (i in 0..19) {
-            for (j in 0..9) {
-                if (blockedCells[19 - i][j]) {
-                    canvas.drawRect(
-                        j * cellWidth + cellPadding,
-                        i * cellWidth + cellPadding,
-                        j * cellWidth + cellWidth,
-                        i * cellWidth + cellWidth,
-                        blockedCellPaint
-                    )
-                } else {
-                    canvas.drawRect(
-                        j * cellWidth + cellPadding,
-                        i * cellWidth + cellPadding,
-                        j * cellWidth + cellWidth,
-                        i * cellWidth + cellWidth,
-                        cellPaint
-                    )
-                }
-                if (19 - i == roverPosition.y && j == roverPosition.x) {
-                    canvas.drawBitmap(
-                        getRoverBitmap(),
-                        null,
-                        roverRect,
-                        null
-                    )
-                }
-            }
-        }
-        textBubbleRect?.let {
-            canvas.drawBitmap(textBubble, null, it, null)
-            canvas.drawText(message, textPos!!.first, textPos!!.second, messagePaint)
-        }
-
-    }
-
-    private var textBubbleRect: RectF? = null
-    private var textPos: Pair<Float, Float>? = null
-    private var message: String = ""
-
 
     fun showMessage(cellXY: Point, message: String) {
         val bounds = Rect()
@@ -142,16 +90,18 @@ class RoverView : View {
         var right = left + (bounds.width() + 100)
         var bottom = top + 150
 
+        // let's check that our message bubble doesn't exceeds the view boundaries
+        // vertically
         if (top < 0) {
             top += abs(top)
             bottom = top + 200
         }
 
+        // horizontally
         if (right > measuredWidth) {
             left -= right - measuredWidth
             right = left + (bounds.width() + 100)
         }
-
 
         textBubbleRect = RectF(
             left,
@@ -166,6 +116,40 @@ class RoverView : View {
 
     }
 
+    fun processCommand(commands: String) {
+        textBubbleRect = null
+
+        // we don't want to freeze the UI while waiting for our timeout
+        thread {
+            for (c in commands) {
+                if (cancelMovement) {
+                    cancelMovement = false
+                    return@thread
+                }
+
+                // let's check that our view hasn't gone away while we were waiting
+                if (handler == null) {
+                    return@thread
+                }
+                when (c) {
+                    'M' -> handler.post { moveOneCell() }
+                    'R' -> handler.post { turnRight() }
+                    'L' -> handler.post { turnLeft() }
+                }
+                Thread.sleep(300)
+            }
+        }
+    }
+
+    private fun calculateRoverRect() {
+        roverRect = RectF(
+            roverPosition.x * cellWidth + cellPadding,
+            (19 - roverPosition.y) * cellWidth + cellPadding,
+            roverPosition.x * cellWidth + cellWidth,
+            (19 - roverPosition.y) * cellWidth + cellWidth
+        )
+    }
+
     private fun getRoverBitmap() = when (direction) {
         UP -> roverUp
         RIGHT -> roverRight
@@ -174,7 +158,7 @@ class RoverView : View {
         else -> throw RuntimeException("Invalid direction")
     }
 
-    private var direction = UP
+
     private fun turnRight() {
         direction = when (direction) {
             UP -> RIGHT
@@ -216,7 +200,6 @@ class RoverView : View {
         invalidate()
     }
 
-    private var cancelMovement = false
     private fun checkPath(): Boolean {
         val nextPos = Point(roverPosition)
         when (direction) {
@@ -235,25 +218,54 @@ class RoverView : View {
         }
     }
 
-    fun processCommand(commands: String) {
-        textBubbleRect = null
-        thread {
-            for (c in commands) {
-                if (cancelMovement) {
-                    cancelMovement = false
-                    return@thread
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        if (measuredWidth > 0 && measuredHeight > 0) {
+            cellWidth = min((measuredWidth - 11 * cellPadding) / 9, (measuredHeight - 11 * cellPadding) / 19)
+            setMeasuredDimension(measuredWidth, measuredHeight)
+            viewRect.set(0f, 0f, measuredWidth.toFloat(), measuredHeight.toFloat())
+            calculateRoverRect()
+        }
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        // let's create a 10x20 grid
+        for (i in 0..19) {
+            for (j in 0..9) {
+                if (blockedCells[19 - i][j]) {
+                    canvas.drawRect(
+                        j * cellWidth + cellPadding,
+                        i * cellWidth + cellPadding,
+                        j * cellWidth + cellWidth,
+                        i * cellWidth + cellWidth,
+                        blockedCellPaint
+                    )
+                } else {
+                    canvas.drawRect(
+                        j * cellWidth + cellPadding,
+                        i * cellWidth + cellPadding,
+                        j * cellWidth + cellWidth,
+                        i * cellWidth + cellWidth,
+                        cellPaint
+                    )
                 }
-                if (handler == null) {
-                    return@thread
+                // here we detect if should draw the rover
+                // and our origin is not the true graphical origin, hence the 19 - x
+                if (19 - i == roverPosition.y && j == roverPosition.x) {
+                    canvas.drawBitmap(
+                        getRoverBitmap(),
+                        null,
+                        roverRect,
+                        null
+                    )
                 }
-                when (c) {
-                    'M' -> handler.post { moveOneCell() }
-                    'R' -> handler.post { turnRight() }
-                    'L' -> handler.post { turnLeft() }
-                }
-                Thread.sleep(300)
             }
         }
+        textBubbleRect?.let {
+            canvas.drawBitmap(textBubble, null, it, null)
+            canvas.drawText(message, textPos!!.first, textPos!!.second, messagePaint)
+        }
+
     }
 
 }
